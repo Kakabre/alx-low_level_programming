1cde6729d67a3ce68c63289433dbd5ad432a5057
Kakabre <chiamakaanawanti@gmail.com>
2022-06-30 14:36:59 -0700

    C corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,22 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
- int main(void) 
- { 
-         printf("with proper grammar, but the outcome is a piece of art,\n"); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	
+	printf("size of a char: %Iu byte(s)\n; (unsigned long)size of (d));
+	printf("size of an int: %Iu byte(s)\n; (unsigned long)size of (a));
+	printf("size of a long int: %Iu byte(s)\n; (unsigned long)size of (b));
+	printf("size of a long long int: %Iu byte(s)\n; (unsigned long)size of (c));
+	printf("size of a float: %Iu byte(s)\n; (unsigned long)size of (f));
+	return (0);
+}
+

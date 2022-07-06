f9233ac4bd89044c4c9eb301c44487e0e4989702
Kakabre <chiamakaanawanti@gmail.com>
2022-07-06 04:36:36 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		if (j!= 20365011074)
 		{
-			printf("%Id,",j);
+			printf("%ld,",j);
 		}
 		else
 		{
-			printf("%Id\n",j);
+			printf("%ld\n",j);
 		}
 		next = j + k;
 		j = k;

b07a65034afca325b995b22c009acd90a0d68dd6
Kakabre <chiamakaanawanti@gmail.com>
2022-07-07 08:28:04 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply to integers
+ *
+ * @a: integer one
+ * @b: integer two
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

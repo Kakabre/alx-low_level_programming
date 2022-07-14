7e163a260284d170fe38fdca7b81ed3985024c94
Kakabre <chiamakaanawanti@gmail.com>
2022-07-14 13:30:58 -0700

    infinte

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,36 +10,33 @@
 
 void print_buffer(char *b, int size)
 {
-	int a, d, c;
+	int byte, index;
 
+	for (byte = 0; byte < size; byte += 10)	{
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				printf(" ");
+			else
+				printf("%02x", *(b + index + byte))
+					if ((index % 2) != 0 && index != 0)
+						printf(" ");
+		}
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
+		printf("\n");
+	}
 	if (size <= 0)
 		printf("\n");
-	else
-	{
-		for (a = 0; a < size; a += 10)
-		{
-			printf("%.8x: ", a);
-			for (d = a; d < a + 10; d++)
-			{
-				if (d % 2 == 0)
-					printf(" ");
-				if (d < size)
-					printf("%.2x", (*b + d));
-				else
-					printf(" ");
-			}
-
-				for (c = a; c < d + 10; c++)
-				{
-					if (c >= size)
-						break;
-					if (*(b + 1) < 32 || *(b + 1) > 126)
-						printf("%c", '.');
-					else
-						printf("%c", *(b + 1));
-				}
-				printf("\n");
-		}
-	}
 }
-

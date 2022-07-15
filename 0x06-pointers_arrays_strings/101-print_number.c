86c418643f8414045fa22c3c3d267be4dd686bd9
Kakabre <chiamakaanawanti@gmail.com>
2022-07-14 17:28:04 -0700

    printimg

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,27 +8,27 @@
 
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int o, e, count;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		o = n * -1;
 	}
 	else
 	{
-		m = n;
+		o = n;
 	}
-	d = m;
+	e = o;
 	count = 1;
 
-	while (d > 9)
+	while (e > 9)
 	{
-		d /= 10;
+		e /= 10;
 		count *= 10;
 	}
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / count) % 10) + 45);
+		_putchar(((o / count) % 10) + 48);
 	}
 }

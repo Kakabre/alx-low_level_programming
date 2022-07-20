1231d9d84b562bdf66fadea5663f04e086b8741b
Kakabre <chiamakaanawanti@gmail.com>
2022-07-20 15:31:51 -0700

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Returns the lenght of a string
+ * @s: string
+ * Return: length of the string
+ */
+
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - checks if a string is a palindrome
+ * @s: string
+ * @len: length of string
+ * @index: index of string to be checked
+ * Return: 1 if is a palindrome
+ * and 0 if it is not
+ */
+
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string
+ *
+ * Return: 1 if is a palindrome
+ * and 0 if it is not
+ */
+
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, len, index));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,6 +4,24 @@ int is_divisible(int n, int d);
 int is_prime_number(int n);
 
 /**
+ * is_divisible - checks if a number is divisible
+ * @n: number to be divided
+ * @d: divisor
+ * Return: 0 if n is divisible and
+ * -1 if otherwise
+ */
+
+int is_divisible(int n, int d)
+{
+	if (n % d == 0)
+		return (0);
+	if (d == n / 2)
+		return (1);
+
+	return (is_divisible(n, d + 1));
+}
+
+/**
  * is_prime_number - determines a prime number
  * @n: integer
  * Return: 1 if n is a prime number
@@ -12,4 +30,14 @@ int is_prime_number(int n);
 
 int is_prime_number(int n)
 {
-	if (n 
+	int d = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n >= 2 && n <= 3)
+		return (1);
+	
+	return (is_divisible(n, d));
+
+}

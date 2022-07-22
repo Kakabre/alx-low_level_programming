b549fe2016e7a355c9c24b62081f374c143b6de9
Kakabre <chiamakaanawanti@gmail.com>
2022-07-22 03:02:45 -0700

    libraries

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int j = 0;
+	int k = 0;
+
+	while (k == 0)
+	{
+		if ((*(s1 + j) == '\0') && (*(s2 + j) == '\0'))
+			break;
+		{
+				k = *(s1 + j) - *(s2 + j);
+				j++;
+		}
+	}
+				return (k);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to the beginning of the located substring or
+ * NULL if none is found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+	while (*haystack)
+	{
+		for (a = 0; needle[a]; a++)
+		{
+			if (haystack[a] != needle[a])
+				break;
+		}
+	if (a != s)
+		haystack++;
+	else
+	return (haystack);
+	}
+
+	return (NULL);
+}

5fb388a6031a89bb84b044812b3bbfe709a8e3cd
Kakabre <chiamakaanawanti@gmail.com>
2022-07-22 06:59:20 -0700

    argument

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void print_number(int num);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int main(int argc __attribute__((unused)), char *argv[]);
+
+#endif

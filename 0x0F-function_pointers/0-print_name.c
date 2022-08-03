d08deed132b400f31a2c176f8492a7a5c92c4205
Kakabre <chiamakaanawanti@gmail.com>
2022-08-03 12:32:56 -0700

    functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to be printed
- * @f: function to print a name
- * Return: void
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - selects the correct function to perform
- * @s: operator passed as argument to the program
- * Return: NULL if s does not match +, -, ÷, *, %
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
+ *
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
  */
-
-int (*get_up_func(char *s))(int, int)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+" = op_add},
-		{"-" = op_sub},
-		{"*" = op_mul},
-		{"/" = op_div},
-		{"%" = op_mod},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i = 0;

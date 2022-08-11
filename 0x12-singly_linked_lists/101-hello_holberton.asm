5c799dd569a13124a2d691b7bc46d042f1fdcf46
Kakabre <chiamakaanawanti@gmail.com>
2022-08-11 13:21:44 -0700

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
-global main
-extern printf
-
-SECTION .text
-main:
-mov rdi, msg
-call printf
-ret
-
-        SECTION .data
-msg:    db "Hello, Holberton", 0
-fmt:    db "%s", 10, 0
-=======
   SECTION .text
   extern printf
   global main
-  
+
   main:
   push rbp
   mov rdi, fmt
   mov rsi, msg
   mov rax, 0
   call printf
-  
+
 pop rbp
 
 mov rax, 0
@@ -30,18 +16,4 @@ ret
 
   SECTION .data
 msg:    db "Hello, Holberton", 0
-fmt:    db "%s", 10, 0
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> 16039185327879a3de27cce4e00bc582b4f40e85
+fmt:    db "%s", 10

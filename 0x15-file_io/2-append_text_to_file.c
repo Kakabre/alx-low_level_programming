148ef00bc5416dcff2db6e450b6c39e0063cef5f
Kakabre <chiamakaanawanti@gmail.com>
2022-08-22 14:14:38 -0700

    write only

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,12 +13,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int len = 0, red = 1;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);

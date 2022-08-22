fe86d5ec0394d313103b45495131c6e161112feb
Kakabre <chiamakaanawanti@gmail.com>
2022-08-22 13:40:10 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,23 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-/**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
 
 /**
  * create_file - creates a file.
@@ -26,20 +7,30 @@ size_t _strlen(char *str)
  *
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len = 0;
+	int len = 0, red = 1;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
+	{
+		close(fd);
+		return (1);
+	}
+	while (text_content[len])
+		len++;
+
+		red = write(fd, text_content, len);
 	close(fd);
-	if (len == -1)
+
+	if (red == -1 || red != len)
 		return (-1);
 	return (1);
 }
